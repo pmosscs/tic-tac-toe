e242db2746a32c2df0e4ba22c439f3c322077c8a
Peyton Moss <pmoss94@gmail.com>
2022-11-11 20:30:26 -0700

    finished project - add tie checker?

--- src/App.js
+++ src/App.js
@@ -3,18 +3,52 @@ import "./App.css";
 import Square from "./components/Square";
 
 function App() {
-  const [squares, setSquares] = useState("", "", "", "", "", "", "", "", "");
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
+    setPlayer(true);
+  };
+
+  const calculateWinner = (array) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (array[a] && array[a] === array[b] && array[a] === array[c]) {
+        return `${array[a]} is the winner!`;
+      }
+    }
+    return "Who will win??";
+  };
 
   return (
     <div className="App">
-      <Square
-        player={player}
-        setPlayer={setPlayer}
-        squares={squares}
-        setSquares={setSquares}
-      />
-      ;
+      <span>{calculateWinner(squares)}</span>
+      <div className="container">
+        {squares.map((val, i) => {
+          return (
+            <Square
+              squareValue={val}
+              index={i}
+              player={player}
+              setPlayer={setPlayer}
+              squares={squares}
+              setSquares={setSquares}
+            />
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
 function Square(props) {
+    const handleClick = () => {
+        if (!props.squareValue) {
+            if (props.player) {
+                props.squares.splice(props.index, 1, "X")
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            } else {
+                props.squares.splice(props.index, 1, "O");
+                props.setSquares(props.squares);
+                props.setPlayer(!props.player);
+            }
+
+        }
+    }
     return (
-        console.log(props.squares, props.player)
+        <div className="square" onClick={handleClick}>{props.squareValue === "O" ? <p>O</p>  : props.squareValue}</div>
     )
 }
 
 export default Square;
+
+// props.squareValue == 0 && props.player == true
